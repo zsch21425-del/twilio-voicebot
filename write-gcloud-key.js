00df16cd8bf9fe770a49c4a09fd4524f48882b0d
zsch21425-del <zsch21425@gmail.com>
2026-04-21 17:38:54 -0400

    Fix syntax error in write-gcloud-key.js

--- write-gcloud-key.js
+++ write-gcloud-key.js
@@ -1,4 +1,4 @@
-——const fs = require('fs');
+—const fs = require('fs');
 const keyPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || '/tmp/gcloud-key.json';
 const json = process.env.GOOGLE_CREDENTIALS_JSON;
 if (json) {
